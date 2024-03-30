71eab80f29ffbcd63115e40e9208ea7c87234651
ifdall26 <lisyukri.ifdal@gmail.com>
2024-03-30 21:05:08 +0700

    Membuat Halaman index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Informasi Perpustakaan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table width="700" border="1">
+        <tr>
+            <td colspan="2" align="center"><h1>Sistem Informasi Perpustakaan</h1></td>
+        </tr>
+        <tr class="tr2">
+            <td width="200">
+                <ul>
+                    <li><a href="anggota.php">Anggota</a></li>
+                    <li><a href="buku.php">Buku</a></li>
+                    <li><a href="pinjam.php">Pinjam</a></li>
+                </ul>
+            </td>
+            <td width="500">Selamat Datang Di Sistem Perpustakaan</td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">Kelompok 1</td>
+        </tr>
+    </table>
+</body>
+</html>
